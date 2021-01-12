a4ee6a14d5f46d7b11205e799ab9e2c87a6a0146
worntunic <tonic.lazar@gmail.com>
2021-01-12 13:13:16 +0100

    UAIGraphData no longer displays all properties

--- Editor/Data/UAIGraphDataEditor.cs
+++ Editor/Data/UAIGraphDataEditor.cs
@@ -8,18 +8,27 @@ namespace UAI.AI.Edit
     [CustomEditor(typeof(UAIGraphData))]
     public class UAIGraphDataEditor : Editor
     {
+        SerializedProperty contextProp;
+        private void OnEnable()
+        {
+            contextProp = serializedObject.FindProperty("context");
+        }
         public override void OnInspectorGUI()
         {
-
+            serializedObject.Update();
             //base.OnInspectorGUI();
+            EditorGUILayout.PropertyField(contextProp);
             UAIGraphData graphData = (UAIGraphData)target;
+            GUI.enabled = graphData.context != null;
             if (GUILayout.Button("Load Graph Editor"))
             {
                 UtilityGraphWindow.OpenEditorWindow(graphData);
             }
-            if (DrawDefaultInspector())
+            GUI.enabled = true;
+            serializedObject.ApplyModifiedProperties();
+            /*if (DrawDefaultInspector())
             {
-            }
+            }*/
 
 
         }
